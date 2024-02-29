461118c0a913f657047c2aa92e4ba30b41106de2
Robin Bergewski <robinbergewski@gmail.com>
2024-02-29 18:11:09 +0100

    do some debugging

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -1,7 +1,5 @@
 # TODO
-[ ] Debugging Python
 [ ] Debugging Zig
-[ ] Debugging Go
 
 [ ] Treesitter swapping
 
--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -4,11 +4,15 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
         "leoluz/nvim-dap-go",
+        "mfussenegger/nvim-dap-python",
         "rcarriga/nvim-dap-ui",
     },
     config = function()
         require("dapui").setup()
+
+        -- they register adapater and configuration automatically
         require("dap-go").setup()
+        require("dap-python").setup()
 
         -- see: :help dap-api
         local dap = require("dap")
@@ -20,51 +24,6 @@ return {
         dap.listeners.before.event_terminated.dapui_config = dapui.close
         dap.listeners.before.event_exited.dapui_config = dapui.close
 
-        -- Adapters are essentially the debuggers one configures
-        -- see :help dap-adapter
-        dap.adapters.python = {
-            type = "executable",
-            command = "python3",
-            args = { "-m", "debugpy.adapter" }
-        }
-        dap.adapters.gdb = {
-            type = "executable",
-            command = "gdb",
-            args = { "-i", "dap" }
-        }
-
-        -- Configurations tell the adapter how we want the file to be debugged
-        -- see :help dap-configuration
-        dap.configurations.python = {
-            {
-                type = 'python',
-                request = 'launch',
-                name = "Launch file",
-                program = function()
-                    local path = vim.fn.input({
-                        prompt = 'Path to executable: ',
-                        default = vim.fn.expand("%:p"), -- current file
-                        completion = 'file'
-                    })
-                    return (path and path ~= "") and path or dap.ABORT
-                end,
-                pythonPath = function()
-                    return vim.cmd("!which python3")
-                end,
-            },
-        }
-        dap.configurations.c = {
-            {
-                name = "Launch",
-                type = "gdb",
-                request = "launch",
-                program = function()
-                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                end,
-                cwd = "${workspaceFolder}",
-                stopAtBeginningOfMainSubprogram = false,
-            },
-        }
 
         -- ==================================
         --            Keymaps

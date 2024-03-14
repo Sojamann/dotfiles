a35a59c98472779a1ba6c0933aab108bb8e2e727
Robin Bergewski <robinbergewski@gmail.com>
2024-03-14 18:10:18 +0100

    make telescope look nice

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -46,6 +46,26 @@ return {
             require('telescope').load_extension("ui-select")
             require('telescope').load_extension("fzf")
 
+            local colors = require("catppuccin.palettes").get_palette()
+            local TelescopeColor = {
+                TelescopeMatching = { fg = colors.flamingo },
+                TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
+                TelescopePromptPrefix = { bg = colors.surface0 },
+                TelescopePromptNormal = { bg = colors.surface0 },
+                TelescopeResultsNormal = { bg = colors.mantle },
+                TelescopePreviewNormal = { bg = colors.mantle },
+                TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+                TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+                TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+                TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
+                TelescopeResultsTitle = { fg = colors.mantle },
+                TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
+            }
+            for hl, col in pairs(TelescopeColor) do
+                vim.api.nvim_set_hl(0, hl, col)
+            end
+
             local t_builtin = require("telescope.builtin")
 
             vim.keymap.set('n', '<leader>?', t_builtin.oldfiles, { desc = '[?] Find recently opened files' })

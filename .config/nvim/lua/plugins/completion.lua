00279072594a5fc66ef4ad61cb6e8270a9d7122e
Robin Bergewski <robinbergewski@gmail.com>
2024-03-17 12:33:09 +0100

    change tmux looks

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -2,23 +2,10 @@ return {
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            {
-                'L3MON4D3/LuaSnip',
-                tag = "v2.*",
-            },
-            'saadparwaiz1/cmp_luasnip',
-
-            -- Adds LSP completion capabilities
-            'hrsh7th/cmp-nvim-lsp',
-
-            -- Adds a number of user-friendly snippets
-            'rafamadriz/friendly-snippets',
+            'L3MON4D3/LuaSnip', -- configured in luasnip.lua
         },
         config = function()
             local cmp = require("cmp")
-
-            require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
                 snippet = {
